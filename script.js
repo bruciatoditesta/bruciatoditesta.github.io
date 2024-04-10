dfdcee1f501f5f2d3312293a993d25cbd3a1c629
Bruno <brunopanzera06@gmail.com>
2024-04-10 17:47:50 +0200

    a
    a

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function mappa()
-{
-    window.location = "mappa.html";
+function zoomin() {
+    let GFG = document.getElementsByClassName('responsive')
+    let currWidth = GFG.clientWidth;
+    GFG.style.width = (currWidth + 100) + "px";
 }
+
+function zoomout() {
+    let GFG = document.getElementsByClassName('responsive')
+    let currWidth = GFG.clientWidth;
+    GFG.style.width = (currWidth - 100) + "px";
+} 
