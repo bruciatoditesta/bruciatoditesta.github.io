80caab1c79c6fa3fcff9f259fddb954f49ccb2ca
Bruno <brunopanzera06@gmail.com>
2024-04-10 17:51:43 +0200

    a
    a

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function zoomin() {
-    let GFG = document.getElementsByClassName('responsive')
-    let currWidth = GFG.clientWidth;
-    GFG.style.width = (currWidth + 100) + "px";
+let image = document.getElementById('immagine');
+function ZoomIn() {
+   let width = image.clientWidth;
+   let height = image.clientHeight;
+   image.style.width = (width + 50) + "px";
+   image.style.height = (height + 50) + "px";
 }
-
-function zoomout() {
-    let GFG = document.getElementsByClassName('responsive')
-    let currWidth = GFG.clientWidth;
-    GFG.style.width = (currWidth - 100) + "px";
-} 
+function ZoomOut() {
+   let width = image.clientWidth;
+   let height = image.clientHeight;
+   image.style.width = (width - 50) + "px";
+   image.style.height = (height - 50) + "px";
+}
